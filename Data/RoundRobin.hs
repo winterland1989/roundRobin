a7cbeecd7bedbd5824724bb637cf4bb6bb961f03
Michael Xavier <michael@michaelxavier.net>
2016-08-06 12:54:25 -0700

    Make user-facing API typesafe
    It wasn't too much harder to make it impossible to violate the main
    invariant of the roundrobin (no empty lists). In accordance with this, I
    also used an explicit export list so the invariants couldn't be violated
    that way.

--- Data/RoundRobin.hs
+++ Data/RoundRobin.hs
@@ -1,6 +1,12 @@
-module Data.RoundRobin where
+module Data.RoundRobin
+    ( RoundRobin
+    , newRoundRobin
+    , select
+    , set
+    ) where
 
 import Data.IORef
+import Data.List.NonEmpty (NonEmpty(..))
 
 -- | A simple round-robin table
 -- useful for selecting resource using round-robin fashion.
@@ -10,16 +16,14 @@ newtype RoundRobin a = RoundRobin (IORef [a]) deriving Eq
 --
 -- If list is empty, an error will be raised.
 -- will use 'NonEmpty' in future(ghc 8 are widely used).
-newRoundRobin :: [a] -> IO (RoundRobin a)
-newRoundRobin xs =
-    if null xs
-    then error "can't create an empty RoundRobin"
-    else newIORef (cycle xs) >>= return . RoundRobin
+newRoundRobin :: NonEmpty a -> IO (RoundRobin a)
+newRoundRobin (x :| xs) =
+    newIORef (cycle (x:xs)) >>= return . RoundRobin
 
 -- | select an item from round-robin table.
 select :: RoundRobin a -> IO a
 select (RoundRobin ref) = atomicModifyIORef' ref (\ (a:as) -> (as, a))
 
 -- | set a new round-robin table.
-set :: RoundRobin a -> [a] -> IO ()
-set (RoundRobin ref) xs = atomicModifyIORef' ref (const (cycle xs , ()))
+set :: RoundRobin a -> NonEmpty a -> IO ()
+set (RoundRobin ref) (x :| xs) = atomicModifyIORef' ref (const (cycle (x:xs) , ()))
